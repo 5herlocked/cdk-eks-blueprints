b87d46601ef9d241b1525a7d07146bd6446cf4ce
Shardul Vaidya <vshardul@amazon.com>
2023-04-21 09:17:27 -0400

    Testing stuff including the secrets store addon

--- bin/main.ts
+++ bin/main.ts
@@ -5,14 +5,14 @@ import * as blueprints from '../lib';
 import {KubernetesVersion, NodegroupAmiType} from "aws-cdk-lib/aws-eks";
 import {AuroraClusterProvider} from "../lib/resource-providers/rds";
 import {AuroraPostgresEngineVersion, DatabaseClusterEngine} from "aws-cdk-lib/aws-rds";
-import {VpcProvider} from "../lib";
+import {GlobalResources, LookupHostedZoneProvider, VpcProvider} from "../lib";
 // import BlueprintConstruct from '../examples/blueprint-construct';
 
 const app = new cdk.App();
 
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
-const props = { env: { account, region } };
+const props = {env: {account, region}};
 
 // new BlueprintConstruct(app, props);
 
@@ -23,21 +23,33 @@ const clusterProvider = new blueprints.GenericClusterProvider({
     {
       id: "mng1",
       amiType: NodegroupAmiType.AL2_X86_64,
-      instanceTypes: [ new ec2.InstanceType('m5a.large') ],
+      instanceTypes: [new ec2.InstanceType('m5a.large')],
       desiredSize: 1,
       maxSize: 2,
-      nodeGroupSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }
+      nodeGroupSubnets: {subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}
     }
   ]
 });
 
+const addOns = [
+  new blueprints.addons.ExternalDnsAddOn({
+    hostedZoneResources: [
+      GlobalResources.HostedZone
+    ]
+  }),
+  new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+  new blueprints.addons.ExternalsSecretsAddOn(),
+  new blueprints.addons.BackstageAddOn(),
+];
+
 blueprints.EksBlueprint.builder()
-  .resourceProvider(blueprints.GlobalResources.Vpc, new VpcProvider(undefined, "20.0.0.0/16", ["20.0.1.0/24","20.0.2.0/24","20.0.3.0/24"]))
+  .resourceProvider(blueprints.GlobalResources.Vpc, new VpcProvider(undefined, "20.0.0.0/16", ["20.0.1.0/24", "20.0.2.0/24", "20.0.3.0/24"]))
   .resourceProvider(blueprints.GlobalResources.Rds, new AuroraClusterProvider({
     auroraEngine: DatabaseClusterEngine.auroraPostgres(
-      { version: AuroraPostgresEngineVersion.VER_14_6 }
+      {version: AuroraPostgresEngineVersion.VER_14_6}
     ),
     name: "auroraRds"
   }))
   .clusterProvider(clusterProvider)
+  .addOns()
   .build(app, 'resourceProvider-test', props);
